15bc763255b10a0540eb8addc7a25d3e2e7fdf90
snowdence <snowdence2911@gmail.com>
2021-07-04 01:28:17 +0700

    Functional Component vs Class Component

---
+++ 01_HelloWorld/hello-world/src/components/Greet.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Greet() {
+  return (
+    <div>
+      <h1> Hello, Snowdence</h1>
+    </div>
+  );
+}
+
+const GreetFunction = () => {
+  return (
+    <div>
+      <h1> Hello 2, Snowdence</h1>
+    </div>
+  );
+};
+
+export { Greet, GreetFunction };
---
+++ 01_HelloWorld/hello-world/src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Class Component here</h1>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
