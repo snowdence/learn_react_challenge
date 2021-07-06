53cd7fe47e1da3cf3d6bfe04a653d08dc9e85b31
snowdence <snowdence2911@gmail.com>
2021-07-06 12:57:52 +0700

    [D3] React Hook 18_1 useRef to handle button clear interval

--- 01_HelloWorld/react-http/src/App.js
+++ 01_HelloWorld/react-http/src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import React, { useReducer } from "react";
 import Parent from "components/start_callback/Parent";
 import FocusInput from "components/start_ref/FocusInput";
+import HookTimer from "components/start_ref/HookTimer";
 
 function App() {
   return (
     <div className="App">
       <h1> App </h1>
-      <FocusInput />
+      <HookTimer />
     </div>
   );
 }
