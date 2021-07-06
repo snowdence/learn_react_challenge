c0429413c0e5977285f71a0d90c2cf5e596e6c4e
snowdence <snowdence2911@gmail.com>
2021-07-06 09:51:57 +0700

    [D3] React Hook 5 array useState and change

--- 01_HelloWorld/react-http/src/App.js
+++ 01_HelloWorld/react-http/src/App.js
@@ -4,11 +4,12 @@ import React from "react";
 import HookCounter from "components/HookCounter";
 import HookCounterTwo from "components/HookCounterTwo";
 import HookCounterThree from "components/HookCounterThree";
+import HookCounterFour from "components/HookCounterFour";
 
 function App() {
   return (
     <div className="App">
-      <HookCounterThree />
+      <HookCounterFour />
     </div>
   );
 }
