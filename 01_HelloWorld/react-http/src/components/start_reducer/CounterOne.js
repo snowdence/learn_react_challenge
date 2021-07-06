d5f31b29e32da51033378581b5b3a399cf5e5fc9
snowdence <snowdence2911@gmail.com>
2021-07-06 11:17:12 +0700

    [D3] React Hook 13 useReducer complex state object counter

--- 01_HelloWorld/react-http/src/components/start_reducer/CounterOne.js
+++ 01_HelloWorld/react-http/src/components/start_reducer/CounterOne.js
@@ -1,28 +1,63 @@
 import React, { useReducer } from "react";
 
-const initialState = 0;
+const initialState = {
+  firstCounter: 0,
+  secondCounter: 0,
+};
 
-const reducerCount = (currentState, action) => {
-  switch (action) {
+const reducerCount = (state, action) => {
+  switch (action.type) {
     case "increment":
-      return currentState + 1;
+      return { ...state, firstCounter: state.firstCounter + action.value };
     case "decrement":
-      return currentState - 1;
+      return { ...state, firstCounter: state.firstCounter - action.value };
+    case "increment2":
+      return { ...state, secondCounter: state.secondCounter + action.value };
+    case "decrement2":
+      return { ...state, secondCounter: state.secondCounter - action.value };
     case "reset":
       return initialState;
     default:
-      return currentState;
+      return state;
   }
 };
 
 function CounterOne() {
-  const [count, dispatchCount] = useReducer(reducerCount, initialState);
+  const [counter, dispatchCount] = useReducer(reducerCount, initialState);
   return (
     <div>
-      <div>Count : {count}</div>
-      <button onClick={() => dispatchCount("increment")}>Increment</button>
-      <button onClick={() => dispatchCount("decrement")}>Decrement</button>
-      <button onClick={() => dispatchCount("reset")}>Reset</button>
+      <div>Count : {counter.firstCounter}</div>
+      <div>Count2 : {counter.secondCounter}</div>
+      <div>
+        <button onClick={() => dispatchCount({ type: "increment", value: 1 })}>
+          Increment
+        </button>
+        <button onClick={() => dispatchCount({ type: "decrement", value: 1 })}>
+          Decrement
+        </button>
+        <button onClick={() => dispatchCount({ type: "increment", value: 5 })}>
+          Increment 5
+        </button>
+        <button onClick={() => dispatchCount({ type: "decrement", value: 5 })}>
+          Decrement 5
+        </button>
+      </div>
+
+      <div>
+        <button onClick={() => dispatchCount({ type: "increment2", value: 1 })}>
+          Increment2
+        </button>
+        <button onClick={() => dispatchCount({ type: "decrement2", value: 1 })}>
+          Decrement2
+        </button>
+        <button onClick={() => dispatchCount({ type: "increment2", value: 5 })}>
+          Increment2 5
+        </button>
+        <button onClick={() => dispatchCount({ type: "decrement2", value: 5 })}>
+          Decrement2 5
+        </button>
+      </div>
+      <button onClick={() => dispatchCount({ type: "reset" })}>Reset</button>
     </div>
   );
 }
