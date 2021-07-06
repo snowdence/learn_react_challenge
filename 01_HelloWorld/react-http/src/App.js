d244cfe2e206bdb2bced2aeb2ce6f059a7a21f9e
snowdence <snowdence2911@gmail.com>
2021-07-06 09:19:10 +0700

    [D3] React HTTP Axios post post

--- 01_HelloWorld/react-http/src/App.js
+++ 01_HelloWorld/react-http/src/App.js
@@ -2,12 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import PostList from "./components/posts/PostList";
+import PostForm from "./components/posts/PostForm";
 
 function App() {
   return (
     <div className="App">
       <h1>Name</h1>
-      <PostList />
+      <PostForm />
     </div>
   );
 }
