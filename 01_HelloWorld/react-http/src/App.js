a45d631f64d916c020df9b6b37988aa28cedd7c6
snowdence <snowdence2911@gmail.com>
2021-07-06 12:13:25 +0700

    [D3] React Hook 13 useReducer + API

--- 01_HelloWorld/react-http/src/App.js
+++ 01_HelloWorld/react-http/src/App.js
@@ -1,39 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useReducer } from "react";
-import ComponentA from "components/context_effect/ComponentA";
-import ComponentB from "components/context_effect/ComponentB";
-import ComponentC from "components/context_effect/ComponentC";
-import ComponentD from "components/context_effect/ComponentD";
+import DataFetchingOne from "components/start_reducer/DataFetchingOne";
 
-const initialState = 0;
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    case "reset":
-      return initialState;
-    default:
-      return state;
-  }
-};
-export const CountContext = React.createContext("count_context");
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
-
   return (
-    <CountContext.Provider
-      value={{ countState: count, countDispatch: dispatch }}
-    >
-      <div className="App">
-        <h1> global {count} </h1>
-        <ComponentA />
-        <ComponentB />
-        <ComponentC />
-      </div>
-    </CountContext.Provider>
+    <div className="App">
+      <h1> App </h1>
+      <DataFetchingOne />
+    </div>
   );
 }
 
---
+++ 01_HelloWorld/react-http/src/components/start_reducer/DataFetchingOne.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import React, { useEffect, useReducer, useState } from "react";
+
+const initialState = {
+  loading: true,
+  error: "",
+  post: {},
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_SUCESS":
+      return { loading: false, error: "", post: action.payload };
+    case "FETCH_ERROR":
+      return {
+        loading: false,
+        error: "Something went wrong",
+        post: {},
+      };
+    default:
+      return state;
+  }
+};
+
+function DataFetchingOne() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/pxosts/1 `)
+      .then((response) => {
+        dispatch({ type: "FETCH_SUCESS", payload: response.data });
+      })
+      .catch((error) => {
+        dispatch({ type: "FETCH_ERROR" });
+      });
+  }, []);
+  const { loading, error, post } = state;
+
+  return (
+    <div>
+      {loading ? "Loading" : post.title}
+      <b style={{ color: "red" }}>{error ? error : ""}</b>
+    </div>
+  );
+}
+
+export default DataFetchingOne;
