bc5caa91ac9489c25338c29bab02b3ea8f1611cd
snowdence <snowdence2911@gmail.com>
2021-07-06 10:02:11 +0700

    [D3] React Hook 7 useEffect conditional rerender

--- 01_HelloWorld/react-http/src/components/start_effect/CounterOne.js
+++ 01_HelloWorld/react-http/src/components/start_effect/CounterOne.js
@@ -2,12 +2,20 @@ import React, { useEffect, useState } from "react";
 
 function CounterOne() {
   const [count, setCount] = useState(0);
-
+  const [title, setTitle] = useState("");
   useEffect(() => {
+    console.log("Update document title");
     document.title = `You clicked ${count} times`;
-  });
+  }, [count]);
+
   return (
     <div>
+      <div>{title}</div>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
       <button onClick={() => setCount(count + 1)}>Increase {count}</button>
     </div>
   );
