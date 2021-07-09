3e6f9b66c9bdc4d07185c14a9e2fcb8c1a0ec8e0
snowdence <snowdence2911@gmail.com>
2021-07-09 17:19:06 +0700

    [D5] React Redux 24  Redux Dev Tools

--- 01_HelloWorld/start-redux/src/redux/store.js
+++ 01_HelloWorld/start-redux/src/redux/store.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import cakeReducer from "./cake/cakeReducer";
 import rootReducer from "./rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import logger from "redux-logger";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
