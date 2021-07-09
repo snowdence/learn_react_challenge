b26513181629d7317adf978c33e2c79412dcf7be
snowdence <snowdence2911@gmail.com>
2021-07-09 12:06:44 +0700

    [D5] React Redux 11 - Middleware Logger

--- 01_HelloWorld/redux-js/index.js
+++ 01_HelloWorld/redux-js/index.js
@@ -1,9 +1,12 @@
 const redux = require("redux");
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = "BUY_CAKE"; // actions types
 const BUY_ICECREAM = "BUY_ICECREAM";
 const createStore = redux.createStore;
 const combineReducer = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
 
 function buyCake() {
   return {
@@ -60,11 +63,10 @@ const rootReducer = combineReducer({
   iceCream: iceCreamReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("initial state", store.getState());
-const unsubscribe = store.subscribe(() =>
-  console.log("Updated state", store.getState())
-);
+const unsubscribe = store.subscribe(() => {});
+//  console.log("Updated state", store.getState()));
 
 store.dispatch(buyCake());
 store.dispatch(buyCake());
